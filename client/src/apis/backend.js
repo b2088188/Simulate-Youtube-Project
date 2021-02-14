32e372f0564ac8efabebdf94f3b0f3611a730e25
Shunze Lin <4A490101@stust.edu.tw>
2021-02-14 13:20:28 +0800

    doc: Renaming some design utils files

--- client/src/apis/backend.js
+++ client/src/apis/backend.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export const authRequest = axios.create({
-   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/auth`
-   //withCredentials: true
+   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/auth`,
+   withCredentials: true
 });
 export const userRequest = axios.create({
-   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/users`
-   //withCredentials: true
+   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/users`,
+   withCredentials: true
 });
 export const channelRequest = axios.create({
    baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/channels`
 });
 
 export const videoRequest = axios.create({
-   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/videos`
-   //withCredentials: true
+   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/videos`,
+   withCredentials: true
 });
